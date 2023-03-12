cae93f1e32094708a29926807d7303fd4bc8704f
Sophorn <31152870+sophorn-thun@users.noreply.github.com>
2023-03-12 22:01:51 +0100

    Update script.js

--- JavaScript01/Week02/script.js
+++ JavaScript01/Week02/script.js
@@ -36,3 +36,13 @@ else {
     highestNumber = a;
 };
 console.log(highestNumber);
+
+/* 6. Arrays */
+let animals = [];
+console.log("the value of my array will be: 'cat', 'dog', 'bunny'");
+console.log(animals);
+animals = ['cat', 'dog', 'bunny'];
+animals.push('baby pig');
+console.log(animals);
+
+
