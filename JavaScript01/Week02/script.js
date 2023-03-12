24d884756252123c56925799aba1155fc7a7d35a
Sophorn <31152870+sophorn-thun@users.noreply.github.com>
2023-03-12 21:22:03 +0100

    Update script.js

--- JavaScript01/Week02/script.js
+++ JavaScript01/Week02/script.js
@@ -3,4 +3,22 @@ console.log('Hello, World!'); //English
 console.log('សួស្តី ពិភពលោក!'); //Khmer
 console.log('Hej, Världen!'); //Swedish
 
+/* 2. Fix syntax error */
+console.log("I'm awesome");
+
+/* 3. Declare a variable x and initialize it with an integer */
+let x;
+console.log("the value of x will be: 10");
+console.log(x);
+x = 10;
+console.log("the value of x will be: 10");
+console.log(x);
+
+/* 4. Declare a variable y and assign a string to it. */
+let y = 'I am a string'
+console.log("the value of my string will be: 'I am a string'");
+console.log(y);
+y = 'I am a new string';
+console.log("the value of my logged string will be: 'I am a new string'");
+console.log(y);
 
