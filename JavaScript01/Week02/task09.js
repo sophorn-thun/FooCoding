28d0eb8e5fa9f51327e9d67b2c41914005d57a5a
Sophorn <31152870+sophorn-thun@users.noreply.github.com>
2023-03-13 19:51:02 +0100

    Create task09.js

---
+++ JavaScript01/Week02/task09.js
@@ -0,0 +1,9 @@
+/* Remainder assignment operator */
+
+console.log("new value of x would be remainder of 7 divided by 3 which is 1");
+
+console.log("if x is equal to 10 and then x is equal to x % 3, then new value of x would be remainder of 10 divided by 3 which is 1");
+
+console.log("if y is equal to 12 and then y is equal to y % 2, then new value of y would be remainder of 12 divided by 2 which is 0");
+
+console.log("if z is equal to 20 and then z is equal to x % 6, then new value of z would be remainder of 20 divided by 6 which is 2");
