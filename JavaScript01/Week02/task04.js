f96fcd2ed8322281071a38ceeb094b70137dd030
Sophorn <31152870+sophorn-thun@users.noreply.github.com>
2023-03-13 16:48:18 +0100

    Create task04.js

---
+++ JavaScript01/Week02/task04.js
@@ -0,0 +1,8 @@
+/* 3. Declare a variable x and initialize it with an integer */
+let x;
+console.log("the value of x will be: 10");
+console.log(x);
+x = 10;
+console.log("the value of x will be: 10");
+console.log(x);
+
