f2ddb20c86c3faae67b71ebe564b9a6005ab2287
Sophorn <31152870+sophorn-thun@users.noreply.github.com>
2023-03-13 20:16:43 +0100

    Create Infinitive.js

---
+++ JavaScript01/Week02/Infinitive.js
@@ -0,0 +1,22 @@
+/* Array */
+
+/* Yes. You can store multiple types of data in an array. Below is an example */
+
+let multipleTypes = ['string', 10, true,  null];
+console.log(multipleTypes);
+
+
+/* Comparing positive infinitives */
+
+let x = 6/0;
+console.log(x);
+let y = 10/0;
+console.log(y);
+
+x === y;
+
+if (x === y) {
+  console.log('positive infinitive is equal');
+} else {
+  console.log('positive infinitive is not equal');
+};
