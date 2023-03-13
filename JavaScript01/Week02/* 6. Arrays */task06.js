3439cebdd7838f6f4e20ea7d491bed2aa8675588
Sophorn <31152870+sophorn-thun@users.noreply.github.com>
2023-03-13 16:54:23 +0100

    Delete JavaScript01/Week02/* 6. Arrays * directory

--- JavaScript01/Week02/* 6. Arrays */task06.js
+++
@@ -1,7 +0,0 @@
-/* 6. Arrays */
-let animals = [];
-console.log("the value of my array will be: 'cat', 'dog', 'bunny'");
-console.log(animals);
-animals = ['cat', 'dog', 'bunny'];
-animals.push('baby pig');
-console.log(animals);
