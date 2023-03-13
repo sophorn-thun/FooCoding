d1bf5c0755c358c823fff0e214858cf10562872e
Sophorn <31152870+sophorn-thun@users.noreply.github.com>
2023-03-13 16:53:04 +0100

    Create task04.js

---
+++ JavaScript01/Week02/task04.js
@@ -0,0 +1,7 @@
+/* 4. Declare a variable y and assign a string to it. */
+let y = 'I am a string'
+console.log("the value of my string will be: 'I am a string'");
+console.log(y);
+y = 'I am a new string';
+console.log("the value of my logged string will be: 'I am a new string'");
+console.log(y);
