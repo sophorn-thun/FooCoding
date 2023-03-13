6100c3b10c0811a7e3399a9737b086ed7e57438a
Sophorn <31152870+sophorn-thun@users.noreply.github.com>
2023-03-13 16:53:27 +0100

    Create task05.js

---
+++ JavaScript01/Week02/task05.js
@@ -0,0 +1,15 @@
+
+/* 5. Round the number 7.25, to the nearest integer */
+let z = 7.25;
+console.log(z);
+let a = Math.round(z);
+console.log(a);
+var highestNumber;
+if (z > a) {
+    highestNumber = z;
+}
+else {
+    highestNumber = a;
+};
+console.log(highestNumber);
+
